58f5e6cd3c22e8559a7be2ece3c63daf883462f5
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-12 12:32:15 +0000

    fixed test

--- test.sh
+++ test.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-
 i=0
 scripts_count=0
 
-if [[ "$0" == "*" && "$#" == "1" ]]; then
+if [[ "$1" == "-a" && "$#" == "1" ]]; then
 	# test all files
 	scripts_count=`ls | grep ".sh" | wc -l`
 	for ((i = 0; i <= $scripts_count; i++)) do
-		if -f "task-$i.sh" then
+		if [[ -f "task-$i.sh" ]]; then
 			"./task-$i.sh"
 		fi
 	done
-elif [[ -f "$0" && "$#" == "1" ]]; then
-	# test given file
-	"./$0"
-elif [[ -f "task-$0.sh" && "$#" == "1" ]]; then
-	# test file based on task id
-	"./task-$0.sh"
+elif [[ -f "$1" && "$#" == "1" ]]; then
+	# test the given file
+	"./$1"
+elif [[ -f "task-$1.sh" && "$#" == "1" ]]; then
+	# test the file based on task id
+	"./task-$1.sh"
 else
 	echo -e "\e[31mInvalid usage\e[0m"
-	echo "Usage 00 (run all test files): ./test.sh *"
+	echo "Usage 00 (run all test files): ./test.sh -a"
 	echo "Usage 01 (run test file): ./test.sh file"
 	echo "Usage 10 (run test file based on task id): ./test.sh #"
 fi
