4482d4d03cf027f68e37cb00289eedbe0e95c82d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-21 18:04:28 +0100

    fix

--- task-13.sh
+++ task-13.sh
@@ -4,9 +4,9 @@ echo "setenv test=1" | ../simple_shell
 echo -e "\n\t it should print error of usage"
 
 echo -e "\n\nTesting [ setenv test 2 ]  in the shell ==============================================\n"
-echo "setenv test 2" | ../simple_shell
-echo -e "\n\tthis should print 2 : $test"
+echo "setenv test 2; $test" | ../simple_shell
+echo -e "\n\tthis should print 2 "
 
 echo -e "\n\nTesting [ unsetenv test ]  in the shell ==============================================\n"
-echo "unsetenv test" | ../simple_shell
-echo -e "\n\tthis should print nothing : $test"
+echo "unsetenv test; $test" | ../simple_shell
+echo -e "\n\tthis should print nothing "
