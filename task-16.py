7376f45a8024f73f61dc27108678b64ec27b4eea
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-23 14:42:50 +0100

    :wrench: Added comment for tests declaration

--- task-16.py
+++ task-16.py
@@ -2,12 +2,18 @@
 from test_runner import run_tests, Check_Types
 
 test_cases = [
+  # Tuples folow this format:
+  #   (input_command, output_text, return_code)
   (
     'echo -e "\e[31mCool |\e[0m"\n',
-    '',
-    Check_Types.Success,
+    'Cool |\n',
     0
   ),
+  (
+    'ls main.h\n',
+    'main.h',
+    0
+  )
 ]
 
 run_tests(test_cases)
