e0e6ce28d17f1157f1840db1992ffc05f9f88cae
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-24 13:53:04 +0100

    update

--- task-14.sh
+++ task-14.sh
@@ -6,31 +6,32 @@ reset=`tput sgr0`
 
 echo -e "${green}\n\nTesting [ cd ../blog_post ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
-echo "cd ../blog_post ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo "cd ../blog_postt ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "Returned : ${cd_status} \n"
 echo -e "\n${red}>>>>> Expected : \n${reset}"
-echo "cd ../blog_post ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+echo "cd ../blog_postt ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
 
 echo -e "${green}\n\nTesting [ cd .. ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
-echo "cd .. ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo "cd .. ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
 echo -e "\n${red}>>>>> Expected : \n${reset}"
-echo "cd .. ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+echo "cd .. ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
 
 echo -e "${green}\n\nTesting [ cd /tmp ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
-echo "cd /tmp ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo "cd /tmp ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
 echo -e "\n${red}>>>>> Expected : \n${reset}"
-echo "cd /tmp ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+echo "cd /tmp ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
 
 
 echo -e "${green}\n\nTesting [ cd - ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
-echo "cd - ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo "cd - ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
 echo -e "\n${red}>>>>> Expected : \n${reset}"
-echo "cd - ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+echo "cd - ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
 
 echo -e "${green}\n\nTesting [ cd ~ ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
-echo "cd ~ ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo "cd ~ ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
 echo -e "\n${red}>>>>> Expected : \n${reset}"
-echo "cd ~ ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+echo "cd ~ ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
