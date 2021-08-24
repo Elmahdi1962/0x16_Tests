b258aff8882b01f279f88ad4dfb1841c3907f286
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-24 14:48:34 +0100

    task-14.sh update

--- task-14.sh
+++ task-14.sh
@@ -7,9 +7,17 @@ reset=`tput sgr0`
 echo -e "${green}\n\nTesting [ cd ../blog_post ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
 echo "cd ../blog_postt ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
-echo -e "Returned : ${cd_status} \n"
 echo -e "\n${red}>>>>> Expected : \n${reset}"
 echo "cd ../blog_postt ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+
+
+echo -e "${green}\n\nTesting [ cd ../0x16_Tests ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "cd ../0x16_Tests ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "cd ../0x16_Tests ; echo "Returned : \$?" ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+
+
 
 echo -e "${green}\n\nTesting [ cd .. ]  in the shell ==============================================\n"
 echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
