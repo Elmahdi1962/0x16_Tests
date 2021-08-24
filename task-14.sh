afbf9826a582312674bcdacb9150a363d69890ed
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-24 13:20:23 +0100

    task 14.sh update and test runner print what it tests in

--- task-14.sh
+++ task-14.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
-echo -e "\n\nTesting [ cd ../blog_post ]  in the shell ==============================================\n"
-echo "cd ../blog_post ; echo 'pwd : ' ; pwd" | ../simple_shell
-echo -e "\n\tif blog_post folder exist in the parent folder then pwd should print the path"
 
-echo -e "\n\nTesting [ cd .. ]  in the shell ==============================================\n"
-echo "cd .. ; echo 'pwd : ' ; pwd" | ../simple_shell
-echo -e "\n\tif it worked pwd should print the path of the parent folder"
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
-echo -e "\n\nTesting [ cd /tmp ]  in the shell ==============================================\n"
-echo "cd /tmp ; echo 'pwd : ' ; pwd" | ../simple_shell
-echo -e "\n\tif it worked pwd should print the path of /tmp"
+echo -e "${green}\n\nTesting [ cd ../blog_post ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "cd ../blog_post ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "cd ../blog_post ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
 
-echo -e "\n\nTesting [ cd - ]  in the shell ==============================================\n"
-echo "cd - ; echo 'pwd : ' ; pwd" | ../simple_shell
-echo -e "\n\tif it worked pwd should print the path of the last path you were on"
+echo -e "${green}\n\nTesting [ cd .. ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "cd .. ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "cd .. ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
 
-echo -e "\n\nTesting [ cd ~ ]  in the shell ==============================================\n"
-echo "cd ~ ; echo 'pwd : ' ; pwd" | ../simple_shell
-echo -e "\n\tif it worked pwd should print the path of your home directory"
+echo -e "${green}\n\nTesting [ cd /tmp ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "cd /tmp ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "cd /tmp ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+
+
+echo -e "${green}\n\nTesting [ cd - ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "cd - ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "cd - ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
+
+echo -e "${green}\n\nTesting [ cd ~ ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "cd ~ ; echo 'pwd : ' ; pwd" | ../simple_shell #the command to test in our shell
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "cd ~ ; echo 'pwd : ' ; pwd" | /bin/sh #test same command in sh shell for comparing results
