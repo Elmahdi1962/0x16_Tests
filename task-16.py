5ceda8b00d6c03858e665f8a308c06ceaa43b0ca
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-24 13:51:31 +0100

    :wrennch: Refactored the test_runner.py and updated task-16.py

--- task-16.py
+++ task-16.py
@@ -1,17 +1,12 @@
 #!/bin/python3
 from test_runner import run_tests
 
-
 test_cases = [
-  (
-    'echo Hello && echo World\n',
-  ),
-  (
-    'ls main.h\n',
-  ),
-  (
-    'ls \'*.h\'\n',
-  )
+  ('echo Hello && echo World\n',),
+  ('||ls main.h\n',),
+  ('ls &&',)
+  ('ls ;',)
+  (';ls',)
 ]
 
 run_tests(test_cases)
