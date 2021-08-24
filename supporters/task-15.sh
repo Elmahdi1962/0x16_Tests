6bec6e4774abfaefbdfe0f00f905e4c0af027bce
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-24 15:31:03 +0100

    new test on task-15

--- supporters/task-15.sh
+++ supporters/task-15.sh
@@ -43,3 +43,14 @@ echo -e "Returned : $?"
 echo -e "\n${red}>>>>> Expected : \n${reset}"
 echo ";echo 65; echo 12"  | /bin/sh #test same command in sh shell for comparing results
 echo -e "Returned : $?"
+
+
+#test4
+echo -e "${green}\n\nTesting [ ec;echo 65; echo 12  ]  in the shell ==============================================\n"
+echo -e "${red}>>>>> OUR Shell Result:\n${reset}"
+echo "ec;echo 65; echo 12" | ../../simple_shell #the command to test in our shell
+echo -e "Returned : $?"
+
+echo -e "\n${red}>>>>> Expected : \n${reset}"
+echo "ec;echo 65; echo 12"  | /bin/sh #test same command in sh shell for comparing results
+echo -e "Returned : $?"
