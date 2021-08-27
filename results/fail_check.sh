c8cba38d5345ec27ca09de6440c559319610d553
Livingstone David Kwame Hini <73464921+DavidLivingstoneHini@users.noreply.github.com>
2021-08-27 13:39:52 +0000

    Create fail_check.sh

---
+++ results/fail_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CHECK_NAME=${1}
+FILL="###########################################"
+SIZE=${#CHECK_NAME}
+MAXSIZE=70
+SIZEOFFILL="$(($MAXSIZE - $SIZE))"
+SIDE="$(($SIZEOFFILL / 2))"
+if [ $(($SIZEOFFILL % 2)) != 0 ]
+then
+	SIDE2=$(($SIDE - 1))
+else
+	SIDE2=$(($SIDE - 2))
+fi
+
+printf "\r\e[1;31m%.${SIDE}s %.${SIZE}s %.${SIDE2}s\e[0m\n" $FILL $CHECK_NAME $FILL
+echo "check" >> ../rcounter
